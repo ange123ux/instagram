1926d2b7e4463d05784434b7c6989de31ce94c46
Ange Uwimana <uuwange@gmail.com>
2021-01-13 18:40:40 +0200

    installed tinymce

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,6 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns=[
     url(r'^$',views.home_images,name = 'homePage'),
     url(r'^new/image$',views.new_image,name = 'new_image'),
